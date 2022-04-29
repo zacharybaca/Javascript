3a05816320982c07d4797b118fac0fa589c3c18a
zacharybaca <bacazach@icloud.com>
2022-04-28 22:44:41 -0500

    created dino constructor function

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 
     // Create Dino Constructor
-
+    function Dino(species, weight, height, diet, where, when, fact) {
+        this.species = species,
+        this.weight = weight,
+        this.height = height,
+        this.diet = diet,
+        this.where = where,
+        this.when = when,
+        this.fact = fact
+    };
 
     // Create Dino Objects
 
