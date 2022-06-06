dee76ede360a9666a0826632a5e1c7693c08bf35
zacharybaca <bacazach@icloud.com>
2022-06-05 22:59:17 -0500

    added on click event for human data function

--- app.js
+++ app.js
@@ -1,107 +1,53 @@
 
     // Create Dino Constructor
-    function Dino(species, image,  weight, height, diet, where, when, fact) {
+    class Dino {
+        constructor(species, weight, height, diet, where, when, fact) {
         this.species = species,
-        this.image = image,
         this.weight = weight,
         this.height = height,
         this.diet = diet,
         this.where = where,
         this.when = when,
-        this.fact = fact
+        this.fact = fact,
+        this.image = "images/" + species.toLowerCase() + ".png"
+        };
     };
 
     // Create Dino Objects
-    let tri = new Dino('Triceratops', './images/triceratops.png', 13000, 114, 'herbavor', 'North America', 'Late Cretaceous', 'First discovered in 1889 by Othniel Charles Marsh');
-    let rex = new Dino('Tyrannosaurus Rex', './images/tyrannosaurus rex.png', 11905, 144, 'carnivor', 'North America', 'Late Cretaceous', 'The largest known skull measures in at 5 feet long.');
-    let ank = new Dino('Anklyosaurus', './images/anklyosaurus.png', 10500, 55, 'herbavor', 'North America', 'Late Cretaceous', 'Anklyosaurus survived for approximately 135 million years');
-    let brach = new Dino('Brachiosaurus', './images/brachiosaurus.png', 70000, 372, 'herbavor', 'North America', 'Late Jurasic', 'An asteroid was named 9954 Brachiosaurus in 1991');
-    let steg = new Dino('Stegosaurus', './images/stegosaurus.png', 11600, 79, 'herbavor', 'North America, Europe, Asia', 'Late Jurasic to Early Cretaceous', 'The Stegosaurus had between 17 and 22 seperate places and flat spines');
-    let ela = new Dino('Elasmosaurus', './images/elasmosaurus.png', 16000, 59, 'carnivor', 'North America', 'Late Cretaceous', 'Elasmosaurus was a marine reptile first discovered in Kansas');
-    let pte = new Dino('Pteranodon', './images/pteranodon.png', 44, 20, 'carnivor', 'North America', 'Late Cretaceous', 'Actually a flying reptile, the Pteranodon is not a dinosaur');
-    let pig = new Dino('Pigeon', './images/pigeon.png', 0.5, 9, 'herbavor', 'World Wide', 'Holocene', 'All birds are living dinosaurs');
+    let dinoInfo = [];
+    fetch("./dino.json")
+        .then(response => response.json())
+        .then(data => dinoInfo = data.Dinos.map(dino => new Dino(dino.species, dino.weight, dino.height, dino.diet, [dino.fact, `My species is ${dino.species}.`, `I belong to ${dino.where}.`, `My when is ${dino.when}.`])));
 
 
-    // Create Human Object
-    let human = {
-    
-    };
 
     // Use IIFE to get human data from form
-    (function getHumanData() {
+    function getHumanData() {
         let humanData = document.querySelectorAll('#dino-compare input');
         human = Array.from(humanData).reduce((acc, input) => ({
             ...acc, [input.id]: input.value
         }), {});
-    }
-    )();
+        console.log(human);
+        return human;
+    };
+    document.getElementById("btn").addEventListener("click", getHumanData);
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-    let dinoArray = {tri, rex, ank, brach, steg, ela, pte, pig};
-    function compareWeight() {
-        for (const [key, value] of Object.entries(dinoArray)) {
-            for (const i in value) {
-             if(i == 'weight'){
-                 console.log(key)
-                 console.log(value[i]);
-             }
-            }
-        }
-    }
+    // // Create Dino Compare Method 1
+    // // NOTE: Weight in JSON file is in lbs, height in inches. 
     
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    function compareHeight() {
-        for (const [key, value] of Object.entries(dinoArray)) {
-            for (const i in value) {
-             if(i == 'height'){
-                 console.log(key)
-                 console.log(value[i]);
-             }
-            }
-        }
-    }
     
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    function compareDiet() {
-        for (const [key, value] of Object.entries(dinoArray)) {
-            for (const i in value) {
-             if(i == 'diet'){
-                 console.log(key)
-                 console.log(value[i]);
-             }
-            }
-        }
-    }
+    // // Create Dino Compare Method 2
+    // // NOTE: Weight in JSON file is in lbs, height in inches.
+    
+    
+    // // Create Dino Compare Method 3
+    // // NOTE: Weight in JSON file is in lbs, height in inches.
+    
 
     // Generate Tiles for each Dino in Array
     
         // Add tiles to DOM
-        let grid = document.getElementById("grid");
-        let div = document.createElement("div");
-        let infoName = document.createElement("h2");
-        let infoImage = document.createElement("img");
-        let infoFact = document.createElement("p");
-        for (const [key, value] of Object.entries(dinoArray)) {
-            for (const i in value) {
-                switch(i) {
-                    case (i == "species"):
-                        infoName.innerHTML = value[i];
-                    case (i == "image"):
-                        infoImage.innerHTML = value[i];
-                    case (i == "fact"):
-                        infoFact.innerHTML = value[i];
-                    default:
-                        break;
-                }
-            }
-            grid.appendChild(div);
-            div.appendChild(infoName);
-            div.appendChild(infoImage);
-            div.appendChild(infoFact);
-        }
+        
 
 
     // Remove form from screen
